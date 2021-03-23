6bc6792fb57497f972051c789abc632d9535d85f
Kasun Niroshana <zapniroshana420@gmail.com>
2021-03-23 12:12:31 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Firebase vide upload',
+      title: 'Firebase video upload',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
